9b6170e221ec8666e5642aa480c28c61463e68a5
matsushita7 <154654555+matsushita7@users.noreply.github.com>
2024-04-01 18:36:29 +0900

    Delete ボカロ専用SNSのコピー directory

--- ボカロ専用SNSのコピー/snsproject/snsapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Coment,Review,Profile,Reply
-# Register your models here.
-
-admin.site.register(Coment)
-admin.site.register(Profile)
-admin.site.register(Review)
-admin.site.register(Reply)
